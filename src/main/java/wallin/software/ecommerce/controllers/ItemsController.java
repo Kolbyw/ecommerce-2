76819fe60923a8a7673ee154f55a58262ff5d98e
Kolby <54323630+Kolbyw@users.noreply.github.com>
2020-02-15 14:43:07 -0600

    API update

--- src/main/java/wallin/software/ecommerce/controllers/ItemsController.java
+++ src/main/java/wallin/software/ecommerce/controllers/ItemsController.java
@@ -1,17 +1,14 @@
 package wallin.software.ecommerce.controllers;
 
-import org.springframework.web.bind.annotation.RequestBody;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import wallin.software.ecommerce.dao.ItemsDao;
@@ -34,5 +31,11 @@ public class ItemsController {
 	public ResponseEntity<List<Item>> getAllItems(){
 		
 		return new ResponseEntity<List<Item>>(itemsDao.retrieveAllItems(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ResponseEntity<Item> insertItem(@RequestParam String name, @RequestParam String description, @RequestParam String price){
+		
+		return new ResponseEntity<Item>(itemsDao.insertItem(name, description, price), HttpStatus.OK);
 	}
 }
--- src/main/java/wallin/software/ecommerce/dao/ItemsDao.java
+++ src/main/java/wallin/software/ecommerce/dao/ItemsDao.java
@@ -19,6 +19,7 @@ public class ItemsDao {
 	private ItemsMapper itemsMapper;
 	private String ITEM_RETRIEVE_SQL = "SELECT \"ID\", \"Name\", \"Description\", \"Price\" FROM \"Commerce\".\"Items\" WHERE \"ID\" = :id";
 	private String ALL_ITEM_RETRIEVE_SQL = "SELECT \"ID\", \"Name\", \"Description\", \"Price\" FROM \"Commerce\".\"Items\"";
+	private String INSERT_SQL = "INSERT INTO \"Commerce\".\"Items\" (\"Name\", \"Description\", \"Price\") VALUES (:name, :description, :price)";
 	
 	public Item retrieveItem(int id) {
 		return namedParameterJdbcTemplate.query(ITEM_RETRIEVE_SQL, new MapSqlParameterSource().addValue("id", id), itemsMapper).get(0);
@@ -26,5 +27,17 @@ public class ItemsDao {
 	
 	public List<Item> retrieveAllItems() {
 		return namedParameterJdbcTemplate.query(ALL_ITEM_RETRIEVE_SQL, itemsMapper);
+	}
+
+	public Item insertItem(String name, String description, String price) {
+		Item item = new Item();
+		item.setName(name);
+		item.setDescription(description);
+		item.setPrice(price);
+		namedParameterJdbcTemplate.update(INSERT_SQL, new MapSqlParameterSource()
+				.addValue("name", item.getName())
+				.addValue("description", item.getDescription())
+				.addValue("price", item.getPrice()));
+		return item;
 	}
 }
